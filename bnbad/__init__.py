d9c1998df38f910450bbb63b95e4f321da75e37f
Tim Menzies <timm@ieee.org>
2020-07-24 10:48:38 -0400

    saving

--- bnbad/__init__.py
+++ bnbad/__init__.py
@@ -4,13 +4,15 @@ Optimizer, written as a data miner.  Break the data up into regions
 of 'bad' and 'better'. Find ways to jump from 'bad' to 'better'.
 Nearly all this processing takes loglinear time.
 
-    :-------:  
-    | Ba    | Bad <----.  planning= (better - bad)
-    |    56 |          |  monitor = (bad - better)
-    :-------:------:   |  
-            | B    |   v  
-            |    5 | Better  
-            :------:  
+```txt
+.-------.  
+| Ba    | Bad <----.  planning= (better - bad)
+|    56 |          |  monitor = (bad - better)
+.-------.------.   |  
+        | B    |   v  
+        |    5 | Better  
+        .------.  
+```
 
 For design details, see the "[about](about)" file.
 
